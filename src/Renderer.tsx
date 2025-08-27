f8e200c80c25ff46f1dd958273c4aae6aa5bdf64
NicolaSDPR1 <Nicola.Paterson@gov.bc.ca>
2025-08-27 09:22:02 -0700

    ADO-3252 separate mode into style and script in renderer to fix non-pdf view

--- src/Renderer.tsx
+++ src/Renderer.tsx
@@ -193,9 +193,10 @@ const Renderer: React.FC<RendererProps> = ({ data, mode, goBack }) => {
   const pdfFormScript = getByType(scripts, 'pdf');
 
   useEffect(() => {
-    const mode = isPrinting ? 'pdf' : 'web';
-    const styleId = `${mode}-form-styles`;
-    const scriptId = `${mode}-form-script`;
+    const modeScript = isPrinting ? 'pdf' : 'web';
+    const modeStyle = (isPrinting && pdfStyleSheet != undefined) ? 'pdf' : 'web';
+    const styleId = `${modeStyle}-form-styles`;
+    const scriptId = `${modeScript}-form-script`;
 
     // Remove any existing style/script tags for both modes
     ['web', 'pdf'].forEach((m) => {
